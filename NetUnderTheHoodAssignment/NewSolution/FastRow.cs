f18f7405264f982faf160ede38aa79a2a59b9e6c
DynamicGame <soliuabdulbasit03@gmail.com>
2024-09-01 19:08:13 +0100

    Add project files.

---
+++ NetUnderTheHoodAssignment/NewSolution/FastRow.cs
@@ -0,0 +1,48 @@
+﻿namespace CsvDataAccess.NewSolution;
+
+public class FastRow
+{
+    private Dictionary<string, int> _intData = new();
+    private Dictionary<string, string> _stringData = new();
+    private Dictionary<string, bool> _boolData = new();
+    private Dictionary<string, decimal> _decimalData = new();
+
+    public void Assign(string columnName, int value)
+    {
+        _intData[columnName] = value;
+    }
+    public void Assign(string columnName, string value)
+    {
+        _stringData[columnName] = value;
+    }
+    public void Assign(string columnName, bool value)
+    {
+        _boolData[columnName] = value;
+    }
+    public void Assign(string columnName, decimal value)
+    {
+        _decimalData[columnName] = value;
+    }
+
+    public object GetAtColumn(string columnName)
+    {
+        if (_intData.ContainsKey(columnName))
+        {
+            return _intData[columnName];
+        }
+        if (_stringData.ContainsKey(columnName))
+        {
+            return _stringData[columnName];
+        }
+        if (_boolData.ContainsKey(columnName))
+        {
+            return _boolData[columnName];
+        }
+        if (_decimalData.ContainsKey(columnName))
+        {
+            return _decimalData[columnName];
+        }
+        return null;
+        
+    }
+}
---
+++ NetUnderTheHoodAssignment/NewSolution/FastTableDataBuilder.cs
@@ -0,0 +1,57 @@
+﻿using CsvDataAccess.CsvReading;
+using CsvDataAccess.Interface;
+using CsvDataAccess.OldSolution;
+using System.Data.Common;
+
+namespace CsvDataAccess.NewSolution;
+
+public class FastTableDataBuilder : ITableDataBuilder
+{
+
+    public ITableData Build(CsvData csvData)
+    {
+        var resultRows = new List<FastRow>();
+
+        foreach (var row in csvData.Rows)
+        {
+            var fastRow = new FastRow();
+
+            for (int columnIndex = 0; columnIndex < csvData.Columns.Length; ++columnIndex)
+            {
+                var column = csvData.Columns[columnIndex];
+                string valueAsString = row[columnIndex];
+                
+                if (string.IsNullOrEmpty(valueAsString))
+                {
+                    continue;
+                }
+                else if (valueAsString == "TRUE")
+                {
+                    fastRow.Assign(column, valueAsString);
+                }
+                else if (valueAsString == "FALSE")
+                {
+                    fastRow.Assign(column, valueAsString);
+                }
+                else if (valueAsString.Contains(".") && decimal.TryParse(valueAsString, out var valueAsDecimal))
+                {
+                    fastRow.Assign(column, valueAsDecimal);
+                }
+                else if (int.TryParse(valueAsString, out var valueAsInt))
+                {
+                    fastRow.Assign(column, valueAsInt);
+                }
+                else
+                {
+                    fastRow.Assign(column, valueAsString);
+                }
+            }
+
+            resultRows.Add(fastRow);
+        }
+
+        return new FastTableData(csvData.Columns, resultRows);
+    }
+
+   
+}
